dee106f2068c7d198831294e381e4410017fb1fd
Miljenko Noršić <miljenko.norsic@biss.hr>
2022-04-27 14:31:55 +0200

    Fixed SpringBootTest WebRestClient initialization by adding RANDOM_PORT into test

--- src/test/java/com/example/demo/SpringNativeJpaDemoApplicationTests.java
+++ src/test/java/com/example/demo/SpringNativeJpaDemoApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringNativeJpaDemoApplicationTests {
 
 	@Autowired
